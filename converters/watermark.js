b76469f7d13f0758b1d2133f52ee00766e0ff4cb
salihaltnok <asalihensar@gmail.com>
2026-08-25 18:15:57 +0300

    font dosyası

--- converters/watermark.js
+++ converters/watermark.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 module.exports = (command, outputPath) => {
+  // Font dosyasının tam yolunu projenin ana dizininden alıyoruz
+  const fontPath = path.join(__dirname, "..", "font.ttf");
+
   command
     .videoFilters(
-      "drawtext=text='Portfolyo':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=w-tw-10:y=h-th-10",
+      `drawtext=fontfile='${fontPath}':text='Portfolyo':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=w-tw-10:y=h-th-10`,
     )
     .toFormat("mp4")
     .save(outputPath);
